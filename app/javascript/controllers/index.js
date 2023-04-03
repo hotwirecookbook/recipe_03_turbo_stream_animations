b042336d76297542b49a2cf9b0010cffcb8359dd
Andrea Rocca <andrea.rocca3@gmail.com>
2023-04-03 21:22:48 +0200

    Add Turbo Stream animations

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import StreamAnimationsController from "./stream_animations_controller"
+application.register("stream-animations", StreamAnimationsController)
